d5bb78cbea6379c5aae364ebaabd0ec9791b3a71
Yegane <yeganehghulami936@gmail.com>
2024-11-25 11:12:19 -0800

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,55 @@
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  GeolocatorPlatform geolocatorPlatform = GeolocatorPlatform.instance;
+  LocationPermission ? permission;
+
+  Future<void> getPermission()async{
+
+    permission = await geolocatorPlatform.checkPermission();
+
+    if(permission == LocationPermission.denied){
+      print("permission denied");
+
+    }else{
+
+      getLocation();
+    }
+  }
+
+  Future<void> getLocation()async{
+    const LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.low,
+      distanceFilter: 1000
+    );
+
+    Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+    print(position);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: (){
+              getPermission();
+            },
+            child: const Text("User Location",
+            style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
